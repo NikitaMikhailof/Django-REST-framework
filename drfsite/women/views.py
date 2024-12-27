b0de94dcf6ed2f1d6cf00b1d114007672d778703
Nikita Mikhailov <mikhailoffnikita2016@yandex.ru>
2024-12-27 14:49:07 +0300

    pagination

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -4,14 +4,21 @@ from .serializers import WomenSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 
+
+class WomenListPagination(PageNumberPagination):
+    page_size = 1
+    page_query_param = 'page_size'
+    max_page_size = 10000
 
 
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
-
+    pagination_class = WomenListPagination
+    
 
 class WomenAPIUpdate(generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all()
